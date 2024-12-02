9fb1a34d54787fe8487e92736e8e78be4659a57c
Korya25 <enthrj3@gmail.com>
2024-12-02 15:10:00 +0200

    refactor FutureBuilderNews

--- lib/domain/presentation/widget/news/future_builder_News_list.dart
+++ lib/domain/presentation/widget/news/future_builder_News_list.dart
@@ -7,50 +7,49 @@ import 'package:news_app/domain/presentation/widget/news/news_card_sliver_list.d
 class FutureBuilderNews extends StatelessWidget {
   const FutureBuilderNews({super.key, this.catogray});
   final String? catogray;
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Articlemodel>>(
       future:
           NewsApiServices(Dio()).fetchArticles(catogray: catogray ?? 'general'),
       builder: (context, snapshot) {
-        // chack ConnectionState
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const SliverToBoxAdapter(
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-        // Has Data
-
-        else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-          // If data is available
-          return NewsCardList(
-            articles: snapshot.data!,
-          );
-        }
-        // Has Error
-
-        else if (snapshot.hasError) {
-          // If there is an error fetching data
-          return const SliverToBoxAdapter(
-            child: Center(
-              child: Text("Error: Failed to load data"),
-            ),
-          );
-        }
-
-        // No articles found
-        else {
-          // If no data available
-          return const SliverToBoxAdapter(
-            child: Center(
-              child: Text("No articles found"),
-            ),
-          );
+        // Using a switch to handle different states
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const SliverToBoxAdapter(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return _errorState("Error: Failed to load data");
+            } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              return NewsCardList(articles: snapshot.data!);
+            } else {
+              return _errorState("No articles found");
+            }
+          default:
+            return const SliverToBoxAdapter(
+              child: Center(
+                child: Text("Unexpected state"),
+              ),
+            );
         }
       },
+    );
+  }
+
+  // Method to show an error message
+  Widget _errorState(String message) {
+    return SliverToBoxAdapter(
+      child: Center(
+        child: Text(
+          message,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        ),
+      ),
     );
   }
 }
